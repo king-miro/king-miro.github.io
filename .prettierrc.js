542a890564495a9e1bd2d7e128196000a7b0cab7
ro mi <romi@roui-MacBookAir.local>
2026-01-08 14:57:48 +0900

    Updates

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  printWidth: 100,
+  tabWidth: 2,
+  useTabs: false,
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'all',
+  bracketSpacing: true,
+  arrowParens: 'always',
+  proseWrap: 'never',
+  endOfLine: 'auto',
+  plugins: ['prettier-plugin-tailwindcss'],
+}
